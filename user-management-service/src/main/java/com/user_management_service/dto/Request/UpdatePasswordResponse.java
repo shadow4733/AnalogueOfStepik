980a3f65ddc679166831f6839226945011441d3b
shadow4733 <bogdan.rubanik28@gmail.com>
2024-06-21 01:25:41 +0700

    added new Response

---
+++ user-management-service/src/main/java/com/user_management_service/dto/Request/UpdatePasswordResponse.java
@@ -0,0 +1,6 @@
+package com.user_management_service.dto.Request;
+
+public record UpdatePasswordResponse(
+
+) {
+}
--- user-management-service/src/main/java/com/user_management_service/dto/Response/UserResponse.java
+++ user-management-service/src/main/java/com/user_management_service/dto/Response/UserResponse.java
@@ -1,4 +1,4 @@
-package com.user_management_service.dto;
+package com.user_management_service.dto.Response;
 
 
 public record UserResponse(
