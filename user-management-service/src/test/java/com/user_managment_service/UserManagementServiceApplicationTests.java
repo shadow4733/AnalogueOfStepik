cfa9d96dc04ac49fda43e208ecfbce6d3c724992
shadow4733 <bogdan.rubanik28@gmail.com>
2024-06-24 17:01:53 +0700

    refactored code for build(gradle)

--- user-management-service/src/test/java/com/user_managment_service/UserManagementServiceApplicationTests.java
+++ user-management-service/src/test/java/com/user_managment_service/UserManagementServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.user_managment_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UserManagementServiceApplicationTests.class)
 class UserManagementServiceApplicationTests {
 
     @Test
